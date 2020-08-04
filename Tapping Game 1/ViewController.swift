7ccd985f05fd32b493f6d806ef44a85b198e0dad
Emily <2023zhaoe83@gmail.com>
2020-08-04 11:59:12 -0500

    Initial Commit

---
+++ Tapping Game 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tapping Game 1
+//
+//  Created by Emily on 8/4/20.
+//  Copyright © 2020 Emily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
