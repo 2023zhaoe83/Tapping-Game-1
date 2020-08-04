70b77f54850f74af0a1429055e124b3da9d3908b
Eshaan Gandhi <focusoncourt@icloud.com>
2020-08-04 15:33:49 -0400

    hooked up outlets

---
+++ Tapping Game 1/GameEasyViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameEasyViewController.swift
+//  Tapping Game 1
+//
+//  Created by Eshaan Gandhi on 8/4/20.
+//  Copyright © 2020 Emily. All rights reserved.
+//
+
+import UIKit
+
+class GameEasyViewController: UIViewController {
+
+    @IBOutlet weak var easyScoreLabel: UILabel!
+    @IBOutlet weak var easyTimeLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Tapping Game 1/GameMediumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameMediumViewController.swift
+//  Tapping Game 1
+//
+//  Created by Eshaan Gandhi on 8/4/20.
+//  Copyright © 2020 Emily. All rights reserved.
+//
+
+import UIKit
+
+class GameMediumViewController: UIViewController {
+
+    @IBOutlet weak var mediumScoreLabel: UILabel!
+    @IBOutlet weak var mediumTimeLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
