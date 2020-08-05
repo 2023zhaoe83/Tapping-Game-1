784a9e21be75d0d98c1798806efcb68a661abe0e
Emily <2023zhaoe83@gmail.com>
2020-08-05 11:59:52 -0500

    hard level timer reset works

--- Tapping Game 1/GameHardViewController.swift
+++ Tapping Game 1/GameHardViewController.swift
@@ -12,48 +12,51 @@ class GameHardViewController: UIViewController {
     
     @IBOutlet weak var hardScoreLabel: UILabel!
     @IBOutlet weak var hardTimeLabel: UILabel!
+    var timer = Timer()
+    var timerRunning = false
     var seconds = 10
     var tapInt = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        startTimer()
     }
     
     func startTimer() {
-        let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+        timerRunning = true
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
             self.hardTimeLabel.text = String(self.seconds)
             self.seconds -= 1
             if self.seconds == -1 {
-                timer.invalidate()
+                self.timer.invalidate()
+                self.timerRunning = false
                 self.displayAlert(message: "Game Over")
             }
-            
         }
-    
-   }
-    func resetGame(){
-       hardScoreLabel.text = ""
-   }
-   func displayAlert(message: String){
-       let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
-       let alertAction = UIAlertAction(title: "Game Over!", style: .default){
-           (action) in self.resetGame()
     }
-    alert.addAction(alertAction)
-    present(alert, animated: true, completion: nil)
     
-       
-   }
+    func resetGame(){
+        hardScoreLabel.text = ""
+        hardTimeLabel.text = ""
+        seconds = 10
+        tapInt = 1
+    }
+    
+    func displayAlert(message: String){
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Game Over!", style: .default){
+            (action) in self.resetGame()
+        }
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func tapScore(_ sender: Any) {
         self.hardScoreLabel.text = String(self.tapInt)
         self.tapInt += 1
+        if !timerRunning {
+            startTimer()
+        }
     }
-    
-    
-    
-    
-    
-    
 }
 
 
