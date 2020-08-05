53ffdd25aa9202e805af4bcc6ab901cf59a13fc0
Dariush Mokhlesi <dariushmokhlesi@gmail.com>
2020-08-05 10:25:21 -0500

    timer

--- Tapping Game 1/GameEasyViewController.swift
+++ Tapping Game 1/GameEasyViewController.swift
@@ -12,10 +12,25 @@ class GameEasyViewController: UIViewController {
 
     @IBOutlet weak var easyScoreLabel: UILabel!
     @IBOutlet weak var easyTimeLabel: UILabel!
-    override func viewDidLoad() {
+    var seconds = 30
+    
+    
+    
+    
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+            startTimer()
+    }
+    
+    func startTimer() {
+        let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            self.easyTimeLabel.text = String(self.seconds)
+            self.seconds -= 1
+            if self.seconds == 0 {
+                self.timer.invalidate()
+            }
+        }
     }
     
 
