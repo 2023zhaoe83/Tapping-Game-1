a3e7227b05a65461f1af8a7434349ddd9462614d
Eshaan Gandhi <focusoncourt@icloud.com>
2020-08-06 14:13:16 -0400

    added music!

--- Tapping Game 1/ViewController.swift
+++ Tapping Game 1/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var musicEffect: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       let musicFile = Bundle.main.path(forResource: "pixelMusic", ofType: ".mp3")
+       do {
+           try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile!))
+       }
+       catch{
+           print (error)
+       }
     }
-
+    @IBAction func playMusic(_ sender: Any) {
+        musicEffect.play()
+    }
+    @IBAction func musicOff(_ sender: Any) {
+        musicEffect.stop()
+    }
+    
+    
+    
 
 }
 
